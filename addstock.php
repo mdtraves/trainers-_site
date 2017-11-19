e4e8b616379e3d7e9ebdf13f0f3d468ed67e0ba2
mike <mdtraves@hotmail.com>
2017-11-19 22:47:59 +0000

    finalising add stock page, some css styling alterations added

--- addstock.php
+++ addstock.php
@@ -9,11 +9,23 @@
 <div class="maincontent">
         <p><a href="index.php?page=cpanel">Back to admin</a></p>
         <h1>Enter details for new stock item</h1>
-        <form method="post" action="index.php?page=confirmaddstock" enctype="multipart/form-data" />
+        <form method="post" action="index.php?page=confirmaddstock" enctype="multipart/form-data">
         <p><input type="text" name="name" placeholder="Stock item name"/> </p>
-        <p><input type="text" name="thumbnail" placeholder="thumbnail image" /> </p>
-        <p><input type="text" name="name" placeholder="brand" /> </p>
+        <p><input type="file" name="fileToUpload" id="fileToUpload" placeholder="thumbnail image" /></p>
         <p><input type="text" name="price" placeholder="price" /></p>
+        <p><select name="brandID">
+
+        <?php
+        $bralist_sql="SELECT * FROM brand";
+        $bralist_qry=mysqli_query($dbconnect, $bralist_sql);
+        $bralist_rs=mysqli_fetch_assoc($bralist_qry);
+        do { ?>
+            <option value="<?php echo $bralist_rs["brandID"]; ?>"><?php echo $bralist_rs["name"]; ?></option>
+        <?php }   while($bralist_rs=mysqli_fetch_assoc($bralist_qry));
+
+        ?>
+
+            </select></p>
         <p><input type="text" name="topline" placeholder="topline" /></p>
     <p><textarea name="description" placeholder="description" cols=30 rows=5></textarea></p>
 
